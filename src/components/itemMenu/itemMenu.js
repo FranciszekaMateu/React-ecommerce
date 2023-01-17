49b62c3e3cf92652548c1e012de27d15d33bbeb3
FranciszekaMateu <franciszekamataio@gmail.com>
2023-01-17 18:42:08 -0300

    Arreglando menu

--- src/components/itemMenu/itemMenu.js
+++ src/components/itemMenu/itemMenu.js
@@ -15,8 +15,8 @@ const itemMenu = () =>
                 <Accordion.Header><Link to={`/category/cpus`}><h4 className="enlace">Procesadores</h4></Link></Accordion.Header>
                 <AccordionBody>
                     <ListGroup className="opciones">
-                    <ListGroup.Item><Link className="enlace" to={`/category/cpus/amd`}><h4>AMD</h4></Link></ListGroup.Item>
-                    <ListGroup.Item><Link className="enlace" to={`/category/cpus/intel`}><h4>Intel</h4></Link></ListGroup.Item>
+                    <ListGroup.Item className="opcion"><Link className="enlace" to={`/category/cpus/amd`}><h4>AMD</h4></Link></ListGroup.Item>
+                    <ListGroup.Item className="opcion"><Link className="enlace" to={`/category/cpus/intel`}><h4>Intel</h4></Link></ListGroup.Item>
                     </ListGroup>
                 </AccordionBody>
             </AccordionItem>
@@ -24,8 +24,8 @@ const itemMenu = () =>
                 <Accordion.Header><Link className="enlace" to={`/category/perifericos`}><h4>Perifericos</h4></Link></Accordion.Header>
                 <AccordionBody>
                     <ListGroup className="opciones">
-                        <ListGroup.Item><Link className="enlace" to={`/category/perifericos/mouses`}><h4>Mouses</h4></Link></ListGroup.Item>
-                        <ListGroup.Item><Link className="enlace" to={`/category/perifericos/teclados`}><h4>teclados</h4></Link></ListGroup.Item>
+                        <ListGroup.Item className="opcion"><Link className="enlace" to={`/category/perifericos/mouses`}><h4>Mouses</h4></Link></ListGroup.Item>
+                        <ListGroup.Item className="opcion"><Link className="enlace" to={`/category/perifericos/teclados`}><h4>teclados</h4></Link></ListGroup.Item>
                     </ListGroup> 
                 </AccordionBody>
             </AccordionItem>
@@ -33,8 +33,8 @@ const itemMenu = () =>
                 <Accordion.Header><Link className="enlace" to={`/category/gpus`}><h4>Tarjetas graficas</h4></Link></Accordion.Header>
                 <AccordionBody>
                     <ListGroup className="opciones">
-                        <ListGroup.Item><Link className="enlace" to={`/category/gpus/amd`}><h4>AMD</h4></Link></ListGroup.Item>
-                        <ListGroup.Item><Link className="enlace" to={`/category/gpus/nvidia`}><h4>nvidia</h4></Link></ListGroup.Item>
+                        <ListGroup.Item className="opcion"><Link className="enlace" to={`/category/gpus/amd`}><h4>AMD</h4></Link></ListGroup.Item>
+                        <ListGroup.Item className="opcion"><Link className="enlace" to={`/category/gpus/nvidia`}><h4>nvidia</h4></Link></ListGroup.Item>
                     </ListGroup> 
                 </AccordionBody>           
             </AccordionItem>
@@ -42,8 +42,8 @@ const itemMenu = () =>
                 <Accordion.Header><Link className="enlace" to={`/category/moth`}><h4>Placas madre</h4></Link></Accordion.Header>
                 <AccordionBody> 
                     <ListGroup className="opciones">
-                        <ListGroup.Item><Link className="enlace" to={`/category/moth/amd`}><h4>Amd</h4></Link></ListGroup.Item>
-                        <ListGroup.Item><Link className="enlace" to={`/category/moth/intel`}><h4>Intel</h4></Link></ListGroup.Item>
+                        <ListGroup.Item className="opcion"><Link className="enlace" to={`/category/moth/amd`}><h4>Amd</h4></Link></ListGroup.Item>
+                        <ListGroup.Item className="opcion"><Link className="enlace" to={`/category/moth/intel`}><h4>Intel</h4></Link></ListGroup.Item>
                     </ListGroup> 
                 </AccordionBody>
             </AccordionItem>
@@ -51,19 +51,17 @@ const itemMenu = () =>
                 <Accordion.Header><Link className="enlace" to={`/category/harddrive`}><h4 className="funcionando">Almacenamiento</h4></Link></Accordion.Header>
                 <AccordionBody>
                     <ListGroup className="opciones">
-                        <ListGroup.Item><Link className="enlace" to={`/category/hard/hdd`}><h4>Discos duro</h4></Link></ListGroup.Item>
-                        <ListGroup.Item><Link className="enlace" to={`/category/hard/ssd`}><h4>Discos duro solido</h4></Link></ListGroup.Item>
+                        <ListGroup.Item className="opcion"><Link className="enlace" to={`/category/hard/hdd`}><h4>Discos duro</h4></Link></ListGroup.Item>
+                        <ListGroup.Item className="opcion"><Link className="enlace" to={`/category/hard/ssd`}><h4>Discos duro solido</h4></Link></ListGroup.Item>
                     </ListGroup>  
                 </AccordionBody>
             </AccordionItem>
-            <AccordionItem eventKey="6" className="item">
-            <Accordion.Header><Link className="enlace" to={`/category/psu`}><h4 className="funcionando">Fuentes de poder</h4></Link></Accordion.Header>
-            </AccordionItem>
+            <Link className="enlace-nacc" to={`/category/psu`}><h4 className="funcionando">Fuentes de poder</h4></Link>
             <AccordionItem eventKey="7" className="item">
             <Accordion.Header><Link className="enlace" to={`/category/rams`}><h4 className="funcionando">Memorias ram</h4></Link></Accordion.Header>
                 <AccordionBody> 
-                        <ListGroup.Item><Link className="enlace" to={`/category/rams/ddr3`}><h4>Memorias ddr3</h4></Link></ListGroup.Item>
-                        <ListGroup.Item><Link className="enlace" to={`/category/rams/ddr4`}><h4>Memorias ddr4</h4></Link></ListGroup.Item>
+                        <ListGroup.Item className="opcion"><Link className="enlace" to={`/category/rams/ddr3`}><h4>Memorias ddr3</h4></Link></ListGroup.Item>
+                        <ListGroup.Item className="opcion"><Link className="enlace" to={`/category/rams/ddr4`}><h4>Memorias ddr4</h4></Link></ListGroup.Item>
                 </AccordionBody>
             </AccordionItem>
         </Accordion>
